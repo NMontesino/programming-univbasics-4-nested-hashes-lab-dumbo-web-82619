9378be9c8a66702d977588502d5a98a9bd6fa93d
glorious-genre-8033 <namontesino@gmail.com>
2019-08-13 14:22:22 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,8 +81,8 @@ def adding_matz
         }
      }
   programmer_hash[:yukihiro_matsumoto]
-  programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
-  programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
+  programmer_hash[:yukihiro_matsumoto][:known_for] => "Ruby"
+  programmer_hash[:yukihiro_matsumoto][:languages] => ["LISP", "C"]
   programmer_hash
 end
 
@@ -104,7 +104,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
      
 end
 
